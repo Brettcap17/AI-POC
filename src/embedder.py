bdc17e53ea2b27fc27d35e6a50bf6c6fa6493927
Brett Caplan <brett.caplan@appian.com>
2023-08-08 16:19:45 -0400

    Finished code cleanup

--- src/embedder.py
+++ src/embedder.py
@@ -8,11 +8,12 @@ pd.set_option("display.max_columns", None)
 
 # global vars
 dir_path = "output"
-filenames = {}
 question = "How can I change my default search selection in Designer"
 
 # Build a dictionary mapping the filepath of each file to the contents
 def getDictionary():
+  filenames = {}
+
   for file_path in os.listdir(dir_path):
     # check if current file_path is a file
     if os.path.isfile(os.path.join(dir_path, file_path)):
@@ -25,24 +26,27 @@ def getDictionary():
       # map from name to contents
       filenames[file_path] = file_contents
 
-  vector_matrix = addQuestion()
-  output(vector_matrix)
-
-def addQuestion():
   filenames["question"] = question
+  vector_matrix = vectorize(filenames)
+  calculateAndOutput(vector_matrix, filenames)
+
+# Vectorize the file contents
+def vectorize(filenames):
   count_vectorizer = CountVectorizer()
   vector_matrix = count_vectorizer.fit_transform(list(filenames.values()))
   return vector_matrix
 
-def create_dataframe(matrix, tokens):
+def create_dataframe(matrix, tokens, filenames):
   doc_names = list(filenames.keys())
   df = pd.DataFrame(data=matrix, index=doc_names, columns=tokens)
   return df
 
-def output(vector_matrix):
+# Calculate and output the cosine similarity matrix between the question
+# and the files
+def calculateAndOutput(vector_matrix, filenames):
   start_time = time.time()
   cosine_similarity_matrix = cosine_similarity(vector_matrix)
-  df = create_dataframe(cosine_similarity_matrix, list(filenames.keys()))
+  df = create_dataframe(cosine_similarity_matrix, list(filenames.keys()), filenames)
   end_time = time.time()
   print("Asked question: ", question)
   print("Total time (in sec):", end_time - start_time)
