5d309fabf69b33278b32b8788a09bda7b06f1f51
Brett Caplan <brett.caplan@appian.com>
2023-08-08 13:45:42 -0400

    Adding converter script and all initially converted files

---
+++ converter.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+from markdown import markdown
+import os
+
+def main():
+  outputDir = '../output'
+
+  if not os.path.exists(outputDir):
+    os.mkdir(outputDir)
+
+  dirs = os.listdir('../docs/collection_docs/')
+  for i in range(5):
+    dir ='../docs/collection_docs/' + dirs[-i] + '/'
+    for file in os.listdir(dir):
+      print(dir + file)
+      if file.endswith('.md'):
+        fileOut = open(f'{outputDir}/{file.removesuffix(".md")}.txt', 'w')
+
+        fileContents = open(dir + file, 'r').read()
+        html = markdown(fileContents)
+        text = ''.join(BeautifulSoup(html).findAll(text=True))
+
+        fileOut.write(text)
+        fileOut.close()
+
+main()
