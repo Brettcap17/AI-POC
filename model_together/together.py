5ceced357d027eb57026e0236f39e3af91ece1cd
Sid Lakkoju <sid.lakkoju@appian.com>
2023-08-08 12:42:05 -0400

    Added some code

---
+++ model_together/together.py
@@ -0,0 +1,74 @@
+import requests
+from secrets import api_key
+
+
+def start():
+    url = "https://api.together.xyz/instances/start?model=togethercomputer%2FLLaMA-2-7B-32K"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    response = requests.post(url, headers=headers)
+    print(response.text)
+
+
+def ask_question(question):
+    url = "https://api.together.xyz/inference"
+
+    payload = {
+        "model": "togethercomputer/LLaMA-2-7B-32K",
+        "prompt": f"{question}",
+        "max_tokens": 128,
+        "stop": ".",
+        "temperature": 0.7,
+        "top_p": 0.7,
+        "top_k": 50,
+        "repetition_penalty": 1
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+
+    # print(response.json())
+
+    print(response.json()["output"]["choices"][0]["text"])
+
+def stop():
+    url = "https://api.together.xyz/instances/stop?model=togethercomputer%2FLLaMA-2-7B-32K"
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    response = requests.post(url, headers=headers)
+    print(response.text)
+
+
+
+if __name__ == "__main__":
+
+#     with open('test_docs.txt', 'r') as file:
+#         documentation_text = file.read()
+
+
+#     PRE_PROMPT = f"""
+# <human>: Please answer my questions based on the following documentation. No need to respond with anything till I ask a quesetion. {documentation_text}
+# <bot>: My name is Bot, model version is 0.16, part of an open-source kit for fine-tuning new bots! I was created by Together, LAION, Ontocord and the open-source community. I am not human, not evil and not alive, and thus have no thoughts and feelings, but I am programmed to be helpful, polite, honest, and friendly. I will answer you questions about this documentation!
+# """
+
+#     # print(PRE_PROMPT)
+#     start()
+#     ask_question("PRE_PROMPT")
+
+#     while True:
+#         user_input = input()
+
+#         user_prompt = f"""
+# <human>: {user_input}
+# <bot>:"""
+#         print(user_prompt)
+#         ask_question(user_prompt)
+
+    stop()
